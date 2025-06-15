9873102c970754523afadaab7def9bc223feeaa1
marhjd <aussenstelle+github@posteo.de>
2025-06-15 12:27:59 +0200

    Change theme to tokyonight

--- init.lua
+++ init.lua
@@ -882,7 +882,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
